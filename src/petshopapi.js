346cf413cdd98669f0ec59be71560da22995860e
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-04-25 12:50:28 +0300

    Fixed fetch URLS -Aatos

--- src/petshopapi.js
+++ src/petshopapi.js
@@ -1,5 +1,5 @@
 export const fetchProducts = () => {
-    return fetch(import.meta.env.VITE_API_URL)
+    return fetch(import.meta.env.VITE_API_PRODUCTS)
         .then(response => {
             if (!response.ok)
                 throw new Error("Error in fetch: " + response.statusText);
@@ -9,7 +9,7 @@ export const fetchProducts = () => {
 }
 
 export const fetchManufacturers = () => {
-    return fetch(import.meta.env.VITE_API_URL)
+    return fetch(import.meta.env.VITE_API_MANUFACTURERS)
         .then(response => {
             if (!response.ok)
                 throw new Error("Error in fetch: " + response.statusText);

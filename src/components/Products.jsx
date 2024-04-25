ba239a6236f108b178b04d102970201c3186316f
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-04-25 13:42:13 +0300

    Created rudimentary Products.jsx page. Incoherent ramblings on FrontPage.jsx. #6 -Aatos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { AgGridReact } from 'ag-grid-react';
+import { fetchProducts } from "../petshopapi";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 export default function Products() {
-    return(
-    <Typography>
-    <h1 className="container">Add some products here</h1>
-    </Typography>
+
+    const [products, setProducts] = useState([]);
+
+    const [colDef] = useState([
+        { field: 'name', filter: true },
+        { field: 'color', filter: true, width: 100 },
+        { field: 'size', filter: true, width: 100 },
+        { field: 'price', filter: true, width: 100 },
+        { field: 'manufacturer', filter: true, width: 150 },
+        { field: 'productType', filter: true, width: 150 },
+    ]);
+
+    useEffect(() => {
+        handleFetch();
+    }, []);
+
+    const handleFetch = () => {
+        fetchProducts()
+            .then(data => setProducts(data._embedded.products))
+            .catch(err => console.error(err))
+    }
+
+    return (
+        <>
+            <div className="ag-theme-material" style={{ height: 600 }}>
+                <AgGridReact
+                    rowData={products}
+                    columnDefs={colDef}
+                    pagination={true}
+                    paginationAutoPageSize={true}
+                    suppressCellFocus={true}
+                />
+            </div>
+        </>
     );
-  }
+}

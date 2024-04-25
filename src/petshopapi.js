f213ddd704fdbeed03392fe502d4cb51377c7db5
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-04-25 12:49:21 +0300

    Created .env and petshopapi.js #5 -Aatos

--- src/petshopapi.js
+++ src/petshopapi.js
@@ -7,3 +7,13 @@ export const fetchProducts = () => {
             return response.json();
         })
 }
+
+export const fetchManufacturers = () => {
+    return fetch(import.meta.env.VITE_API_URL)
+        .then(response => {
+            if (!response.ok)
+                throw new Error("Error in fetch: " + response.statusText);
+
+            return response.json();
+        })
+}

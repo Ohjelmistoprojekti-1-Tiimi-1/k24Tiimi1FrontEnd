c3f6d0a76bc1911aa0e3182c4b7b2105fbb4d0dc
viscidbog <124368715+viscidbog@users.noreply.github.com>
2024-04-25 13:43:54 +0300

    added singup functionality basic version to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,25 +4,39 @@ import Typography from "@mui/material/Typography";
 import { Stack, Button } from "@mui/material";
 
 export default function Login() {
+  // objekti loginia varten
   // eslint-disable-next-line no-unused-vars
   const [logins, setLogins] = useState({
     username: "",
     password: "",
   });
 
-  const [users, setUsers] = useState([]);
+  // objekti signuppia varten
+  // eslint-disable-next-line no-unused-vars
+  const [signups, setSignups] = useState([
+    {
+      newUsername: "",
+      newPassword: "",
+    },
+  ]);
 
-  const handleClick = () => {
+  const handleLoginClick = () => {
     if (logins.username && logins.password) {
-      setUsers([...users, logins]);
       setLogins({ username: "", password: "" });
+    }
+  };
+
+  // onClick signuppia varten
+  const handleSignupClick = () => {
+    if (signups.newUsername && signups.newPassword) {
+      setSignups({ newUsername: "", newPassword: "" });
     }
   };
 
   return (
     <>
       <Typography variant="h4" sx={{ p: 2 }}>
-        Enter login credentials:
+        Login:
       </Typography>
       <Stack direction="row" sx={{ pl: 2 }} spacing={2}>
         <TextField
@@ -39,8 +53,35 @@ export default function Login() {
           placeholder="Password"
           onChange={(e) => setLogins({ ...logins, password: e.target.value })}
         />
-        <Button variant="contained" onClick={handleClick}>
+        <Button variant="contained" onClick={handleLoginClick}>
           Login
+        </Button>
+      </Stack>
+
+      <Typography variant="h4" sx={{ p: 2 }}>
+        Sign Up:
+      </Typography>
+      <Stack direction="row" sx={{ pl: 2 }} spacing={2}>
+        <TextField
+          value={signups.newUsername}
+          id="outlined-basic"
+          label="username"
+          placeholder="Username"
+          onChange={(e) =>
+            setLogins({ ...signups, newUsername: e.target.value })
+          }
+        />
+        <TextField
+          value={signups.newPassword}
+          id="outlined-basic"
+          label="password"
+          placeholder="Password"
+          onChange={(e) =>
+            setLogins({ ...signups, newPassword: e.target.value })
+          }
+        />
+        <Button variant="contained" onClick={handleSignupClick}>
+          Sign Up
         </Button>
       </Stack>
     </>

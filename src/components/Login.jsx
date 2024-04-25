eb52784b5d25eedab95e4aca9e0e7e20076252fc
viscidbog <124368715+viscidbog@users.noreply.github.com>
2024-04-25 13:30:36 +0300

    added login page functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { Stack, Button } from "@mui/material";
 
 export default function Login() {
   // eslint-disable-next-line no-unused-vars
@@ -9,24 +9,40 @@ export default function Login() {
     username: "",
     password: "",
   });
+
+  const [users, setUsers] = useState([]);
+
+  const handleClick = () => {
+    if (logins.username && logins.password) {
+      setUsers([...users, logins]);
+      setLogins({ username: "", password: "" });
+    }
+  };
+
   return (
     <>
-      <Typography variant="h4">Enter login credentials:</Typography>
-      <Box>
+      <Typography variant="h4" sx={{ p: 2 }}>
+        Enter login credentials:
+      </Typography>
+      <Stack direction="row" sx={{ pl: 2 }} spacing={2}>
         <TextField
           value={logins.username}
           id="outlined-basic"
           label="username"
           placeholder="Username"
-          variant="outlined"
+          onChange={(e) => setLogins({ ...logins, username: e.target.value })}
         />
         <TextField
           value={logins.password}
           id="outlined-basic"
           label="password"
           placeholder="Password"
+          onChange={(e) => setLogins({ ...logins, password: e.target.value })}
         />
-      </Box>
+        <Button variant="contained" onClick={handleClick}>
+          Login
+        </Button>
+      </Stack>
     </>
   );
 }

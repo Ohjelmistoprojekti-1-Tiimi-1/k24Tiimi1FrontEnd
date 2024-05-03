970d24f000640201b06e00e095f5daaa071daf56
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-05-03 12:09:36 +0300

    Updated error page. Added new endpoint to .env. Added new fetch to petshopapi.js -Aatos

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,7 @@
 import { Typography } from "@mui/material";
 import { useRouteError } from "react-router-dom";
+import errorImage from "../assets/errorImage.jpg";
+
 
 export default function Error() {
   const error = useRouteError();
@@ -9,6 +11,7 @@ export default function Error() {
     <Typography>
       <h1>Page not found</h1>
       <p>{error.data}</p>
+      <img src={errorImage} />
     </Typography>
   );
 }
--- src/petshopapi.js
+++ src/petshopapi.js
@@ -8,6 +8,13 @@ export const fetchProducts = () => {
         })
 }
 
+export const fetchProductsWithInfo = async () => {
+    const response = await fetch(import.meta.env.VITE_API_PRODUCTSINFO);
+    if (!response.ok)
+        throw new Error("Error in fetch: " + response.statusText);
+    return await response.json();
+}
+
 export const fetchManufacturers = () => {
     return fetch(import.meta.env.VITE_API_MANUFACTURERS)
         .then(response => {

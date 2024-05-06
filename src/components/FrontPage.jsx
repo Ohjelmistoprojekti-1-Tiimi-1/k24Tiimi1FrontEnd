d116c93f2a2967dc3c59113d7d36e581aaae50fa
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-05-06 09:18:14 +0300

    Removed depricated import -Aatos

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -2,9 +2,6 @@ import { useEffect, useState } from "react";
 import { fetchProducts } from "../petshopapi";
 import ProductCatalog from "./ProductCatalogue.jsx"
 import { Typography } from "@mui/material";
-//import { fetchIndividualProducts } from "../petshopapi";
-
-
 
 function FrontPage() {
 

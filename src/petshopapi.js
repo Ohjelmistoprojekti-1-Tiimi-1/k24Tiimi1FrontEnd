f005162d5472504291550142080bbc5730d3522f
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-05-06 10:08:25 +0300

    Fixing up error messages -Aatos

--- src/petshopapi.js
+++ src/petshopapi.js
@@ -1,7 +1,7 @@
 export const fetchProducts = async () => {
     const response = await fetch(import.meta.env.VITE_API_PRODUCTS);
     if (!response.ok)
-        throw new Error("Error in fetch: " + response.statusText);
+        throw new Error("Error fetching products: " + response.statusText);
     return await response.json();
 }
 
@@ -9,7 +9,7 @@ export const fetchProducts = async () => {
 export const fetchManufacturers = async () => {
     const response = await fetch(import.meta.env.VITE_API_MANUFACTURERS);
     if (!response.ok)
-    throw new Error("Error when fetching manufacturers: " + response.statusText);
+    throw new Error("Error fetching manufacturers: " + response.statusText);
 return await response.json();
 }
 
@@ -17,15 +17,15 @@ return await response.json();
 export const fetchProductsWithInfo = async () => {
     const response = await fetch(import.meta.env.VITE_API_PRODUCTSINFO);
     if (!response.ok)
-        throw new Error("Error in fetch: " + response.statusText);
+        throw new Error("Error fetching products: " + response.statusText);
     return await response.json();
 }
 
 
 export const fetchManufacturerProducts = async (manufacturerName) => {
     const parsedManufacturerName = manufacturerName.replace("/\s/g", "+")
-    const response = await fetch(import.meta.env.VITE_API_MANUFACTURERPRODUCTSWITHTYPE + parsedManufacturerName)
+    const response = await fetch(import.meta.env.VITE_API_MANUFACTURERPRODUCTSWITHTYPE + parsedManufacturerName);
     if (!response.ok)
-        throw new Error("Error when fetching manufacturer's products")
+        throw new Error("Error fetching manufacturer's products: " + response.statusText);
     return await response.json();
 }

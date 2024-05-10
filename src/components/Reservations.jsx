062fd64083f33061bbb4a1eff5d54c14fa5b80cf
pinkkila <petteri.pinkkila@outlook.com>
2024-05-10 21:20:53 +0300

    Added Snacbar to the Reservations

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,39 +1,48 @@
-import React, {useEffect, useState} from "react";
-import {Button, Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Button, Box, Typography, Snackbar } from "@mui/material";
+import { Link } from "react-router-dom";
 import ProductsGrid from "./ProductsGrid";
 import { newReservation } from "../petshopapi";
 
 export default function Reservations() {
     const [currentReservations, setCurrentReservations] = useState([]);
-    const [error, setError] = useState("")
+    const [error, setError] = useState("");
+    const [open, setOpen] = useState(false);
 
-    useEffect (() =>  {
+    useEffect(() => {
         let storage = JSON.parse(sessionStorage.getItem("reservations"));
         if (storage === null) {
-            setCurrentReservations([])
+            setCurrentReservations([]);
         }
-        setCurrentReservations(storage)
-    }, [])
+        setCurrentReservations(storage);
+    }, []);
 
     const reserveProducts = async () => {
         try {
             const message = await newReservation(currentReservations);
-            setError(message)
+            setError(message);
             sessionStorage.setItem("reservations", JSON.stringify([]));
             setCurrentReservations([]);
-        }catch (err) {
-            setError(err.message)
+        } catch (err) {
+            console.error(err.message);
+            setOpen(true)
         }
-    }
+    };
 
     return (
-        <Box>
+        <>
             <Box className="currentReservations">
                 <Typography variant="h3">Current reservation</Typography>
                 <ProductsGrid reserveButtonTrue={false} products={currentReservations} height={300} ></ProductsGrid>
-                <Button onClick={reserveProducts} sx= {{marginLeft: "1em"}}variant="contained">Reserve products above</Button>
-                <Typography color={"error"} >{error}</Typography>
+                <Button onClick={reserveProducts} sx={{ marginLeft: "1em" }} variant="contained">Reserve products above</Button>
             </Box>
-        </Box>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                onClose={() => setOpen(false)}
+                message={"Please login first to make the reservatioin: "}
+                action={<Link to={"/login"} style={{ color: "white" }} >To Login Page</Link>}
+            />
+        </>
     );
 }

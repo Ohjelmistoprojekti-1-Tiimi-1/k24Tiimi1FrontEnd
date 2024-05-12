5c2703b4305ae969dc3afa8ecd1fb72a265b9488
pinkkila <petteri.pinkkila@outlook.com>
2024-05-12 20:50:17 +0300

    Added reservation cacellation functionality

--- src/components/CustomerReservations.jsx
+++ src/components/CustomerReservations.jsx
@@ -1,49 +1,62 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
-import { groupBy, sumBy } from "lodash";
-import { Button, Snackbar, Typography } from "@mui/material";
-import ReservationContext from "./ReservationContext";
-import { newReservation } from "../petshopapi";
-import { Link } from "react-router-dom";
+import { Typography, Button } from "@mui/material";
 import { fetchReservations } from "../petshopapi";
+import dayjs from "dayjs";
+import { fetchCancelReservation } from "../petshopapi";
 
 const CustomerReservations = () => {
-  const [reservations, setReservations] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [productURL, setProductURL] = useState("");
-  const [colDef] = useState([
-    { field: "reservationId", filter: true, width: 150 },
-    { field: "created", filter: true, width: 250 },
-    { field: "delivered", filter: true, width: 250 },
-    { field: "cancelled", filter: true, width: 250 },
-  ]);
+    const [reservations, setReservations] = useState([]);
+    const [colDef] = useState([
+        { field: "reservationId", filter: true, width: 150 },
+        { field: "created", valueGetter: p => dayjs(p.data.created).format('DD.MM.YYYY HH:mm'), filter: true, width: 250 },
+        { field: "delivered", valueGetter: p => p.data.delivered ? dayjs(p.data.delivered).format('DD.MM.YYYY HH:mm') : "", filter: true, width: 250 },
+        { field: "cancelled", valueGetter: p => p.data.cancelled ? dayjs(p.data.cancelled).format('DD.MM.YYYY HH:mm') : "", filter: true, width: 250 },
+        {
+            cellRenderer: params => {
+                if (!params.data.delivered && !params.data.cancelled) {
+                    return (
+                        <Button size="small" color="error" onClick={() => cancelReservation(params.data.reservationId)}>
+                            Cancel
+                        </Button>);
+                }
+            }
+            , width: 120
+        }
+    ]);
 
-  useEffect(() => {
-    handleReservationFetch();
-  }, []);
+    useEffect(() => {
+        handleReservationFetch();
+    }, []);
 
-  const handleReservationFetch = () => {
-    fetchReservations()
-      .then((data) => setReservations(data))
-      .catch((err) => console.error(err));
-  };
+    const handleReservationFetch = () => {
+        fetchReservations()
+            .then((data) => setReservations(data))
+            .catch((err) => console.error(err));
+    };
 
-  return (
-    <>
-      <Typography variant="h4">Your past reservations</Typography>
-      <div className="ag-theme-material" style={{ height: 600 }}>
-        <AgGridReact
-          rowData={reservations}
-          columnDefs={colDef}
-          pagination={true}
-          paginationAutoPageSize={true}
-          suppressCellFocus={true}
-        />
-      </div>
-    </>
-  );
+    const cancelReservation = (reservationId) => {
+        fetchCancelReservation(reservationId)
+            .then(handleReservationFetch)
+            .catch(err => console.error(err));
+    };
+
+    return (
+        <>
+            <Typography variant="h4">Your past reservations</Typography>
+            <div className="ag-theme-material" style={{ height: 600 }}>
+                <AgGridReact
+                    rowData={reservations}
+                    columnDefs={colDef}
+                    pagination={true}
+                    paginationAutoPageSize={true}
+                    suppressCellFocus={true}
+                />
+            </div>
+        </>
+    );
 };
 
 export default CustomerReservations;
--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -6,34 +6,37 @@ import ReservationProducts from "./ReservationProducts";
 import CustomerReservations from "./CustomerReservations";
 
 export default function Reservations() {
-  const [value, setValue] = useState("1");
-  const { reservationProducts, addToReservation } =
-    useContext(ReservationContext);
+    const [value, setValue] = useState("1");
+    const { reservationProducts, addToReservation } = useContext(ReservationContext);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  useEffect(() => {
-    if (reservationProducts.length > 0) setValue("2");
-  }, []);
+    useEffect(() => {
+        if (reservationProducts.length > 0) {
+            setValue("2");
+        } else {
+            setValue("1")
+        }
+    }, [reservationProducts]);
 
-  return (
-    <>
-      <TabContext value={value}>
-        <Box>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Your reservations" value="1" />
-            <Tab label="New Reservation" value="2" />
-          </TabList>
-          <TabPanel value="1">
-            <CustomerReservations />
-          </TabPanel>
-          <TabPanel value="2">
-            <ReservationProducts />
-          </TabPanel>
-        </Box>
-      </TabContext>
-    </>
-  );
+    return (
+        <>
+            <TabContext value={value}>
+                <Box>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                        <Tab label="Your reservations" value="1" />
+                        <Tab label="New Reservation" value="2" />
+                    </TabList>
+                    <TabPanel value="1">
+                        <CustomerReservations />
+                    </TabPanel>
+                    <TabPanel value="2">
+                        <ReservationProducts />
+                    </TabPanel>
+                </Box>
+            </TabContext>
+        </>
+    );
 }
